15faa48cfff67d10b2fbb5cdd07464599dcaffc9
bgrusdt <grusdt@uni-potsdam.de>
2019-05-07 00:12:43 +0200

    updated run-model for using general model

--- analysis/run-model.R
+++ analysis/run-model.R
@@ -2,11 +2,15 @@ library(rwebppl)
 library(tidyverse)
 
 # Parameters --------------------------------------------------------------
-model_filename <- "sundowners"
+#model_filename <- "sundowners"
 #model_filename <- "skiing"
-utterance <- "R > -S"
-#utterance <-  "E > S"
+model_filename <- "model-with-sampling"
 
+#utterance <- "R > -S"
+#utterance <-  "E > S"
+utterance <- "A > C"
+
+bias <- "lawn"
 # Setup -------------------------------------------------------------------
 target_path <- file.path(".",
                          "data",
@@ -18,15 +22,15 @@ model_path <- file.path(".",
                         paste(model_filename, "wppl", sep="."),
                         fsep = .Platform$file.sep)
 
-params <- list(utt=utterance) 
+params <- list(utt=utterance, bias=bias) 
 
 # Run and save model ------------------------------------------------------
 
 posterior <- webppl(program_file = model_path,
                     data = params,
-                    data_var = "data") %>%
-              map(function(x){as_tibble(x)})
+                    data_var = "data")
+posterior_tibble <- posterior %>% map(function(x){as_tibble(x)})
 
-
-write_rds(posterior, target_path)
+# samples <- posterior %>%  map(function(x){get_samples(x, 1000000)})
+write_rds(posterior_tibble, target_path)
 
