a33d6b8aca63c50475ba862bfd0a0b0f7e019bac
brgrusdt <brgrusdt@rtg5.ikw.uni-osnabrueck.de>
2019-07-16 14:39:14 +0200

    added code for informativity of utterances

--- R/figures.R
+++ R/figures.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(latex2exp)
-# parameter sweep
 
+
+# Distributions for parameter sweep ---------------------------------------
 plot_beta <- function(params, xlab){
   p <- ggplot(data = data.frame(x = c(0, 1)), aes(x)) + scale_y_continuous(breaks = NULL)  
   for(i in seq(1, nrow(params))){
@@ -19,3 +20,17 @@ plot_beta(params, xlab=TeX("$\\theta$"))
 params <- tibble(alpha=1, beta=c(10,15,20), col=rainbow(3))
 plot_beta(params, xlab=TeX("$\\beta$"))
 
+
+
+# Plot informativity of utterances
+
+data_dir <- file.path(".", "data", "precomputations", "model-general",
+                      fsep = .Platform$file.sep)
+
+path_tables <- file.path(data_dir, "tables-all.rds", fsep = .Platform$file.sep)
+tables <- read_rds(path_tables) %>% filter(n_tables==500 & noise_v==500)
+
+fn_utts <- paste("utterances-none.rds", sep="")
+path_utterances <- file.path(data_dir, fn_utts, fsep = .Platform$file.sep)
+utterances <- read_rds(path_utterances)
+                                             
