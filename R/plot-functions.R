fd3d22fdeab71315d3adaba8a3584995f22c196f
brgrusdt <brgrusdt@rtg5.ikw.uni-osnabrueck.de>
2019-08-13 17:52:29 +0200

    udpated figures

--- R/plot-functions.R
+++ R/plot-functions.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(scales)
+library(latex2exp)
 
 colors <- hue_pal()(3)
 level2color <- tribble(~level, ~col,
@@ -18,14 +19,18 @@ plot_cns <- function(data, level){
                    stat="identity", position="dodge") + 
           facet_wrap(~level) + 
           labs(x="causal nets", y="probability") +
-          theme(axis.text.x = element_text(angle = 90))
+          theme(axis.text.x = element_text(angle = 45, hjust = 1),
+               text = element_text(size= 15),
+               legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
   }else{
     col <- level2color %>% filter(level== (!!level)) %>% pull(col)
     p <- df %>% filter(level==(!! level)) %>% 
           ggplot() + 
           geom_bar(mapping = aes(x=cn, y=prob), fill=col, stat="identity") + 
           labs(x="causal nets", y="probability") +
-          theme(axis.text.x = element_text(angle = 90))
+          theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                text = element_text(size= 15),
+                legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
   }
   return(p)
 }
@@ -35,6 +40,8 @@ plot_density <- function(df, xlab, level, evs){
       p <- df %>%  ggplot() + 
                     geom_density(mapping = aes(x=support, col=level),
                                  adjust = 6) +
+                    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                                                text = element_text(size= 15)) + 
                     labs(x=xlab, y="density")
       if(!is.null(evs)){
         p <- p + geom_vline(data=evs, aes(xintercept=ev, color=level),
@@ -46,7 +53,8 @@ plot_density <- function(df, xlab, level, evs){
         ggplot() + 
         geom_density(mapping = aes(x=support, col=col),  adjust = 6) + 
         labs(title=level, x=xlab, y="probability") +
-        theme(axis.text.x = element_text(angle = 90), legend.position = "none")
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none",
+              text = element_text(size= 15))
       
       if(!is.null(evs)){
         evs <- evs %>% filter(level == (!!level)) 
@@ -92,22 +100,24 @@ plot_marginal_bar <- function(data, val_marginal_str, level=NULL, save_as="plot-
                stat="identity", position="dodge") + 
       # facet_wrap(~level) + 
       labs(x=xlab, y="probability", title=title) +
-      theme(axis.text.x = element_text(angle = 90))
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+            text = element_text(size= 15),
+            legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
   }else{
     col <- level2color %>% filter(level== (!!level)) %>% pull(col)
     p <- df %>% filter(level==(!! level)) %>% 
       ggplot() + 
       geom_bar(mapping = aes(x=p, y=prob), fill=col, stat="identity") + 
       labs(x=xlab, y="probability", title=level) +
-      theme(axis.text.x = element_text(angle = 90))
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+            text = element_text(size= 15),
+            legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
   }
   if(!is.null(save_as)){
     ggsave(save_as, p)
   }
   return(p)
 }
-
-
 
 plot_voi_alpha_cost <- function(data, model, key, level){
   df <- data %>% filter(model_id==model & key==(!! key) & level== (!! level)) %>% 
@@ -123,12 +133,6 @@ plot_voi_alpha_cost <- function(data, model, key, level){
     p <- p + facet_wrap(~dir)
   }
   print(p)
-  
-  # df %>% ggplot(aes(x=alpha, y=value, col=cost)) +
-  #         geom_point() +
-  #         scale_x_continuous(breaks = data$alpha %>% unique()) +
-  #         ylab(key) +
-  #         ggtitle(level)
 }
 
 plot_evs <- function(data, marginal, save_as){
@@ -141,4 +145,25 @@ plot_evs <- function(data, marginal, save_as){
       labs(x="biases", y=paste("E[P(", marginal, ")]", sep=""))
     ggsave(save_as, p)
     return(p)
+}
+
+plot_cp_vois <- function(data){
+  # data needs column order!
+  data <- data %>% mutate(key_val=case_when(startsWith(key, "cp_bns") ~ 0,
+                                            startsWith(key, "cp_cns") ~ 1,
+                                            key=="p_nc_given_na" ~ 2,
+                                            TRUE ~ -1))
+  
+  p <- data %>% mutate(value=as.numeric(value)) %>% ggplot() + 
+    geom_bar(aes(x=key_val, y=value, fill=level, group=order), stat="identity", position="dodge") + 
+    facet_wrap(~bias) +
+    labs(y="", x="") +
+    scale_x_continuous(breaks=c(0,1, 2),
+                       labels=c(TeX('$\\mathbf{E}(f(X))$'),
+                                TeX('$hel(P_{cp}, P_{cns})$'),
+                                TeX('$P(\\neg C| \\neg A)$'))) + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+          text = element_text(size= 15),
+          legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
+  return(p)
 }
