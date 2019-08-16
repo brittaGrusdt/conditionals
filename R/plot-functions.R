d3431a332a74291f71d3ed7c4faee72df1b854a4
brgrusdt <brgrusdt@rtg5.ikw.uni-osnabrueck.de>
2019-08-16 15:58:23 +0200

    udpated figures

--- R/plot-functions.R
+++ R/plot-functions.R
@@ -9,19 +9,48 @@ level2color <- tribble(~level, ~col,
                        "LL", colors[[1]], 
                        "PL", colors[[2]])
 
-plot_cns <- function(data, level){
-  df <- data %>% group_by(level, cn) %>% summarize(prob=sum(prob))
+plot_bns <- function(data_wide){
+  df <- data_wide %>% group_by(level, bn_id) %>% summarize(prob=sum(prob))
   df$level <- factor(df$level, levels = c("prior", "LL", "PL"))
+    p <- df %>% 
+      ggplot() + 
+      geom_bar(mapping = aes(x=bn_id, y=prob, fill=level),
+               stat="identity", position="dodge") + 
+      facet_wrap(~level) + 
+      labs(x="Bayes nets", y="probability") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+            text = element_text(size= 15),
+            legend.position = "bottom", legend.title = element_blank(),
+            legend.direction = "horizontal")
+    return(p)
+}
+
+plot_cns <- function(data_wide, level=NULL, save_as=NULL){
+  df <- data_wide %>% group_by(level, cn) %>% summarize(prob=sum(prob))
+
+  df$level <- factor(df$level, levels = c("prior", "LL", "PL"))
+  
+  df$cn_nice <- case_when(df$cn=="A implies C"  ~ "A->C",
+                          df$cn=="A implies -C" ~ "A->¬C",
+                          df$cn=="-A implies C" ~ "¬A->C",
+                          df$cn=="-A implies -C" ~ "¬A->¬C",
+                          df$cn=="C implies A" ~ "C->A",
+                          df$cn=="C implies -A" ~ "C->¬A",
+                          df$cn=="-C implies A" ~ "¬C->A",
+                          df$cn=="-C implies -A" ~ "¬C->¬A",
+                          df$cn=="A || C" ~ "A ind. C",
+                          TRUE~"") 
   if(is.null(level)){
     p <- df %>% 
           ggplot() + 
-          geom_bar(mapping = aes(x=cn, y=prob, fill=level),
+          geom_bar(mapping = aes(x=cn_nice, y=prob, fill=level),
                    stat="identity", position="dodge") + 
           facet_wrap(~level) + 
           labs(x="causal nets", y="probability") +
           theme(axis.text.x = element_text(angle = 45, hjust = 1),
                text = element_text(size= 15),
-               legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
+               legend.position = "none", legend.title = element_blank(),
+               legend.direction = "horizontal")
   }else{
     col <- level2color %>% filter(level== (!!level)) %>% pull(col)
     p <- df %>% filter(level==(!! level)) %>% 
@@ -32,6 +61,7 @@ plot_cns <- function(data, level){
                 text = element_text(size= 15),
                 legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
   }
+  if(!is.null(save_as)){ggsave(save_as, p)}
   return(p)
 }
 
@@ -39,7 +69,7 @@ plot_density <- function(df, xlab, level, evs){
   if(is.null(level)){
       p <- df %>%  ggplot() + 
                     geom_density(mapping = aes(x=support, col=level),
-                                 adjust = 6) +
+                                 adjust = 5) +
                     theme(axis.text.x = element_text(angle = 45, hjust = 1),
                                                 text = element_text(size= 15)) + 
                     labs(x=xlab, y="density")
@@ -51,7 +81,7 @@ plot_density <- function(df, xlab, level, evs){
       col <- level2color %>% filter(level == (!!level)) %>% pull(col)
       p <- df %>% filter(level==(!! level)) %>% 
         ggplot() + 
-        geom_density(mapping = aes(x=support, col=col),  adjust = 6) + 
+        geom_density(mapping = aes(x=support, col=col),  adjust = 5) + 
         labs(title=level, x=xlab, y="probability") +
         theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none",
               text = element_text(size= 15))
@@ -66,44 +96,47 @@ plot_density <- function(df, xlab, level, evs){
   return(p)
 }
 
-plot_marginal_prob <- function(data_wide, val_marginal, level=NULL, evs=NULL, 
-                               save_as=NULL){
-  if(val_marginal == "cns"){
-    p <- plot_cns(data_wide, level)
-  } else {
-    samples <- list()
-    for(lev in unique(data_wide$level)){
-      d <- data_wide %>% filter(level==lev)
-      s <- get_samples(tibble(support=d$p, prob=d$prob), 5000000)
-      samples[[`lev`]] <- s %>% add_column(level=lev)
-    }
-    data_marginal <- bind_rows(samples)
-    xlab <- paste("P(", paste(val_marginal, collapse = ","), ")", sep="")
-    p <- plot_density(data_marginal, xlab, level, evs)
-  }
+plot_discrete <- function(df, xlab){
+  data <- df %>% group_by(level) %>%  count(support)%>% mutate(obs=sum(n), perc=n/obs)
+  
+  p <- data %>% group_by(level) %>% 
+        ggplot(aes(x = support, y = perc, fill=level)) +
+        geom_bar(stat = "identity", position="dodge") +
+        labs(x=xlab, y="probability") +
+        facet_wrap(~level)
+  return(p)
+}
+
+plot_marginal_prob <- function(data_long, vals_marginal, level=NULL, evs=NULL, save_as=NULL){
+  marginal <- data_long %>% marginalize(vals_marginal)
+  marginal_samples <- sample_webppl_distr(marginal)
+  xlab <- paste("P(", paste(vals_marginal, collapse = ","), ")", sep="")
+  p <- plot_density(marginal_samples, xlab, level, evs)
   if(!is.null(save_as)){ggsave(save_as, p)}
   return(p)
 }
 
 plot_conditional_prob <- function(data, p, level=NULL, evs=NULL){
   df <- compute_cond_prob(data, p)
-  plot_density(df, p, level, evs)
+  data_samples <- sample_webppl_distr(df)
+  p <- plot_density(data_samples, p, level, evs)
+  return(p)
 }
 
-
-plot_marginal_bar <- function(data, val_marginal_str, level=NULL, save_as="plot-marginal-bar.png", title=""){
+plot_evs_bar <- function(data_evs, val_marginal_str, level=NULL, save_as=NULL, title=""){
   xlab <- paste("P(", paste(val_marginal_str, collapse = ","), ")", sep="")
-  df <- data %>% group_by(level, p) %>% summarize(prob=sum(prob))
+  df <- data_evs %>% mutate(level=as.factor(level))
   if(is.null(level)){
     p <- df %>% 
       ggplot() + 
-      geom_bar(mapping = aes(x=p, y=prob, fill=level),
+      geom_bar(mapping = aes(x=level, y=ev, fill=level),
                stat="identity", position="dodge") + 
       # facet_wrap(~level) + 
       labs(x=xlab, y="probability", title=title) +
       theme(axis.text.x = element_text(angle = 45, hjust = 1),
             text = element_text(size= 15),
-            legend.position = "bottom", legend.title = element_blank(), legend.direction = "horizontal")
+            legend.position = "bottom", legend.title = element_blank(),
+            legend.direction = "horizontal")
   }else{
     col <- level2color %>% filter(level== (!!level)) %>% pull(col)
     p <- df %>% filter(level==(!! level)) %>% 
@@ -132,18 +165,6 @@ plot_voi_alpha_cost <- function(data, model, key, level){
     p <- p + facet_wrap(~dir)
   }
   print(p)
-}
-
-plot_evs <- function(data, marginal, save_as=NULL){
-  df <- data %>% filter(p==marginal) 
-    p <- df %>% 
-      ggplot() + 
-      geom_bar(mapping = aes(x=bias, y=ev, fill=level),
-               stat="identity", position="dodge") + 
-      # facet_wrap(~level) + 
-      labs(x="biases", y=paste("E[P(", marginal, ")]", sep=""))
-    if(!is.null(save_as)){ggsave(save_as, p)}
-    return(p)
 }
 
 plot_cp_vois <- function(data, save_as=NULL){
