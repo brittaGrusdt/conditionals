c0de98fc733e9db8fe46a5435001e22df8abf674
brgrusdt <brgrusdt@rtg5.ikw.uni-osnabrueck.de>
2019-08-22 18:18:28 +0200

    updated figures

--- R/figures.R
+++ R/figures.R
@@ -2,19 +2,26 @@ library(tidyverse)
 library(latex2exp)
 
 
+target_dir <- paste("data", "figs", sep=.Platform$file.sep)
+
+
 # Distributions for parameter sweep ---------------------------------------
 plot_beta <- function(params, xlab){
   p <- ggplot(data = data.frame(x = c(0, 1)), aes(x)) + scale_y_continuous(breaks = NULL)  
   for(i in seq(1, nrow(params))){
-    p <- p + stat_function(fun = dbeta, n = 101, args = list(shape1 = params$alpha[[i]], shape2 = params$beta[[i]]), col = params$col[[i]]) + ylab("")
+    p <- p + stat_function(fun = dbeta, n = 101, args = list(shape1 = params$alpha[[i]],
+                                                             shape2 = params$beta[[i]]),
+                           col = params$col[[i]]) + ylab("")
   }
-  p <- p + labs(x = xlab)
-  print(p)
+  p <- p + labs(x = xlab) + theme_classic() + theme(text = element_text(size= 15))
+  return(p)
 }
 
 # plot beta distributions for theta
 params <- tibble(alpha=seq(4, 13), beta=1, col=rainbow(length(seq(4,13))))
-plot_beta(params, xlab=TeX("$\\theta$"))
+# plot_beta(params, xlab=TeX("$\\theta$"))
+p<-plot_beta(tibble(alpha=c(15), beta=1, col=rainbow(length(c(15)))), "")
+ggsave(paste(target_dir, "beta-right.png", sep=.Platform$file.sep), p)
 
 # plot beta distributions for background noise beta
 params <- tibble(alpha=1, beta=c(10,15,20), col=rainbow(3))
