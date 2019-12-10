dbcda8689d11a759e2ccb9baff46a5b12e0d25cd
MalinSpaniol <mspaniol@uos.de>
2019-12-10 12:53:19 +0100

    ideas event handler for key press - still not working

--- magpie-physical-engine/03_custom_views_templates.js
+++ magpie-physical-engine/03_custom_views_templates.js
@@ -12,23 +12,23 @@
 const multi_slider_generator = {
   // we do not want to show the picture in the stimulus container anymore, but in the grid
   // together with the answer_container
-  stimulus_container_gen: function (config, CT) {
+  stimulus_container_gen: function(config, CT) {
     return `<div class='magpie-view'>
                          <h1 class='magpie-view-title'>${config.title}</h1>
                      </div>`;
   },
 
-  answer_container_gen: function (config, CT) {
+  answer_container_gen: function(config, CT) {
     const option1 = config.data[CT].optionRight;
     const option2 = config.data[CT].optionLeft;
-    return `<div class='magpie-multi-slider-grid'>
+    return `<div class='magpie-multi-slider-grid' id='target'>
           <div class='magpie-grid-picture' >
               <img class='image-stretch' src=${config.data[CT].picture}>
           </div>
           <p class='magpie-grid-qud magpie-view-question magpie-view-qud'><strong>${
             config.data[CT].QUD
           }</strong></p>
-          <div class = 'magpie-grid-slider'>
+          <div class ='magpie-grid-slider'>
               <div id='utterance1' class='magpie-view-answer-container'>
                   <p class='magpie-view-question' id = 'question1' >${
                     config.data[CT].allUtterances[0]
@@ -69,7 +69,7 @@ const multi_slider_generator = {
       <button id='next' class='magpie-view-button grid-button magpie-nodisplay'>Next</button>`;
   },
 
-  handle_response_function: function (
+  handle_response_function: function(
     config,
     CT,
     magpie,
@@ -81,91 +81,113 @@ const multi_slider_generator = {
     let response3;
     let response4;
 
-    $(".magpie-view")
-      .append(answer_container_generator(config, CT));
+    $(".magpie-view").append(answer_container_generator(config, CT));
 
     response1 = $("#response1");
     response2 = $("#response2");
     response3 = $("#response3");
     response4 = $("#response4");
 
+    // function for debug
+
+    // $("#response1")
+    //   .onkeypress(function () {
+    //     // var x = event.keyCode;
+    //     // if (x == 97 || 65 || 27) {
+    //     //   // 27 is the ESC key
+    //     //   alert("You pressed the a key!");
+    //     //   alert("Handler for .onkeypress() called.");
+    //     var x = event.charCode || event.keyCode;
+    //     if (x == 111 || x == 79) {
+    //       //o / O
+    //       //x.innerHTML = "The ALT key was pressed!";
+    //       alter("you pressend o");
+    //     }
+    //   });
+
+    $("#target").on("keypress", function() {
+      // var x = event.keyCode;
+      // if (x == 97 || 65 || 27) {
+      //   // 27 is the ESC key
+      //   alert("You pressed the a key!");
+      //   alert("Handler for .onkeypress() called.");
+      var x = event.charCode || event.keyCode;
+      if (x == 111 || x == 79) {
+        //o / O
+        //x.innerHTML = "The ALT key was pressed!";
+        alter("you pressend o");
+      }
+    });
+
+    // $(document)
+    //   .keypress(function (e) {
+    //     if (e.which == 13) {
+    //       alert('Enter was pressed'); // enter pressed
+    //     }
+    //   });
+
+    $("#response1").on("keypress", function(e) {
+      if (e.which == 13) {
+        alert("Enter was pressed"); // enter pressed
+      }
+    });
+
     // check the sliders for all 4 utterance and handle what utterance
     // is shown and what button is shown
-    response1.on("change", function () {
-      $("#button2")
-        .removeClass("magpie-nodisplay");
+    response1.on("change", function() {
+      $("#button2").removeClass("magpie-nodisplay");
     });
 
-    $("#button2")
-      .on("click", function () {
-        $("#utterance1")
-          .toggleClass("magpie-nodisplay");
-        $("#utterance2")
-          .removeClass("magpie-nodisplay");
-        $("#button2")
-          .toggleClass("magpie-nodisplay");
-      });
-
-    response2.on("change", function () {
-      $("#button3")
-        .removeClass("magpie-nodisplay");
+    $("#button2").on("click", function() {
+      $("#utterance1").toggleClass("magpie-nodisplay");
+      $("#utterance2").removeClass("magpie-nodisplay");
+      $("#button2").toggleClass("magpie-nodisplay");
     });
 
-    $("#button3")
-      .on("click", function () {
-        $("#utterance2")
-          .toggleClass("magpie-nodisplay");
-        $("#utterance3")
-          .removeClass("magpie-nodisplay");
-        $("#button3")
-          .toggleClass("magpie-nodisplay");
-      });
-
-    response3.on("change", function () {
-      $("#button4")
-        .removeClass("magpie-nodisplay");
+    response2.on("change", function() {
+      $("#button3").removeClass("magpie-nodisplay");
     });
 
-    $("#button4")
-      .on("click", function () {
-        $("#utterance3")
-          .toggleClass("magpie-nodisplay");
-        $("#utterance4")
-          .removeClass("magpie-nodisplay");
-        $("#button4")
-          .toggleClass("magpie-nodisplay");
-      });
-
-    response4.on("change", function () {
-      $("#next")
-        .removeClass("magpie-nodisplay");
+    $("#button3").on("click", function() {
+      $("#utterance2").toggleClass("magpie-nodisplay");
+      $("#utterance3").removeClass("magpie-nodisplay");
+      $("#button3").toggleClass("magpie-nodisplay");
     });
 
-    $("#next")
-      .on("click", function () {
-        const RT = Date.now() - startingTime; // measure RT before anything else
-        let trial_data = {
-          trial_name: config.name,
-          trial_number: CT + 1,
-          response: [
-          $("#response1")
-            .val(),
-          $("#response2")
-            .val(),
-          $("#response3")
-            .val(),
-          $("#response4")
-            .val()
+    response3.on("change", function() {
+      $("#button4").removeClass("magpie-nodisplay");
+    });
+
+    $("#button4").on("click", function() {
+      $("#utterance3").toggleClass("magpie-nodisplay");
+      $("#utterance4").removeClass("magpie-nodisplay");
+      $("#button4").toggleClass("magpie-nodisplay");
+    });
+
+    response4.on("change", function() {
+      $("#next").removeClass("magpie-nodisplay");
+    });
+
+    $("#next").on("click", function() {
+      const RT = Date.now() - startingTime; // measure RT before anything else
+      let trial_data = {
+        trial_name: config.name,
+        trial_number: CT + 1,
+        response: [
+          $("#response1").val(),
+          $("#response2").val(),
+          $("#response3").val(),
+          $("#response4").val()
         ],
-          RT: RT
-        };
+        RT: RT
+      };
 
-        trial_data = magpieUtils.view.save_config_trial_data(
-          config.data[CT],
-          trial_data
-        );
-        magpie.trial_data.push(trial_data);
-        magpie.findNextView();
-      });
+      trial_data = magpieUtils.view.save_config_trial_data(
+        config.data[CT],
+        trial_data
+      );
+      magpie.trial_data.push(trial_data);
+      magpie.findNextView();
+    });
   }
 };
