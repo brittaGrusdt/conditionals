a572e36afe909d5c79a6606edf72b1652b943154
MalinSpaniol <mspaniol@uos.de>
2020-01-09 12:59:50 +0100

    cleaning of trials

--- experiment/magpie-pilot/04_trials.js
+++ experiment/magpie-pilot/04_trials.js
@@ -5,10 +5,10 @@ var example_trial = [
   {
     picture: "images/S1-121.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   }
@@ -18,130 +18,130 @@ var slider_rating_trials = [
   {
     picture: "images/S1-121.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S7-130.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S8-202.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S10-203.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S12-203.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S15-443.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S20-468.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S22-468.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S30-805.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S32-806.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S34-806.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S42-806.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S44-806.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
@@ -149,90 +149,90 @@ var slider_rating_trials = [
   {
     picture: "images/S54-806.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S55-1006.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S57-1007.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S59-1007.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S63-1039.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S83-1609.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S89-1642.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S93-1674.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   },
   {
     picture: "images/S97-1674.jpg",
     QUD: "How much do you agree with the following statement?",
-    question4: "designated utterance",
     question1: "utterance1",
     question2: "utterance2",
     question3: "utterance3",
+    question4: "designated utterance",
     optionLeft: "fully disagree",
     optionRight: "fully agree"
   }
